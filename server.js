28d8dfa602805d3938cb5d87f93976de6cd83d02
GrexNaObed <gamletfan@gmail.com>
2022-03-14 22:25:23 +0600

    add __dirname

--- server.js
+++ server.js
@@ -1,9 +1,5 @@
-import express from 'express'
-import path, { dirname } from 'path'
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const express = require('express')
+const path = require('path')
 
 const PORT = process.env.PORT || 5000
 const app = express()
