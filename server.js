b816d2f65bc19e93aac7bfed5a3e77c09fe5ae1d
GrexNaObed <gamletfan@gmail.com>
2022-03-14 22:22:01 +0600

    add __dirname

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 import express from 'express'
-import path from 'path'
+import path, { dirname } from 'path'
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const PORT = process.env.PORT || 5000
-
 const app = express()
 
 app.use(express.static(__dirname))
@@ -10,6 +13,7 @@ app.use(express.static(path.resolve(__dirname, './', 'build')))
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './', 'build', 'index.html'))
+    console.log((path.resolve(__dirname, './', 'build')));
 })
 
 
